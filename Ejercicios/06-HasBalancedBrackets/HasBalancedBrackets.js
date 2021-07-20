a54bda976fc55f859f968c7f2b552ccbed7be09d
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-07-20 16:15:19 -0700

    06 finished

--- Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
+++ Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,17 @@
-function HasBalancedBrackets(string) {
-  // Your code here:
-
-}
-
-module.exports = HasBalancedBrackets;
+const hasBalancedBrackets = (string) => {
+  //  Tenemos un diccionario para chequear las opening brackets con sus closing brackets
+  const validBrackets = {
+      '{' : '}',
+      '[' : ']',
+      '(' : ')',  
+  };
+  const openBrackets = [];
+  for (let i = 0; i < string.length; i += 1) {
+      if (validBrackets[string[i]]) {
+         openBrackets.push(string[i]);
+      } else if (validBrackets[openBrackets.pop()] !== string[i]) {
+         return false;
+      }
+  }
+  return !openBrackets.length;
+};
