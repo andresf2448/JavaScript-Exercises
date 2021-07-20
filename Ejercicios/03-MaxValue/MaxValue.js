f9c85047fa9f6446c873e74b3ebe96bb1cfe3753
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-07-20 16:04:20 -0700

    03 finished

--- Ejercicios/03-MaxValue/MaxValue.js
+++ Ejercicios/03-MaxValue/MaxValue.js
@@ -1,4 +1,10 @@
-function MaxValue(shares) {
-  // Your code here:
-
-}
+const maxValue = (stocks) => {
+  let max = stocks[1] - stocks[0];
+  for (let i = 0; i < stocks.length - 1; i += 1) {
+      for (let j = i + 1; j < stocks.length; j += 1){
+          const potential = stocks[j] - stocks[i];
+          max = potential > max ? potential : max;    
+      }  
+  }
+  return max;
+};
