1d97a25eb7b84ab542141d2e60fd90169b7f4741
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-07-20 16:07:22 -0700

    04 finished

--- Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,6 +1,8 @@
-function mdArraySum(arr) {
-  // Your code here:
-
+const mdArraySum = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+      if (Array.isArray(arr[i])) sum += mdArraySum(arr[i])
+      else sum += arr[i];
+  }
+  return sum;
 }
-
-module.exports = mdArraySum;
