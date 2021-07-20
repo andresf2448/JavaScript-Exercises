17e8a597346336f9a69d3442c6846653f212c32a
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-07-20 16:01:30 -0700

    09 finished

--- Ejercicios/09-BinaryToDecimal/BinaryToDecimal.js
+++ Ejercicios/09-BinaryToDecimal/BinaryToDecimal.js
@@ -1,6 +1,17 @@
 function BinaryToDecimal(binary) {
   // Your code here:
-
+  var acum = 0;
+  for(let i = 0; i < binary.length; i++){
+    acum += Number(binary[i])*Math.pow(2, binary.length - i - 1);
+  }
+  
+  return acum;
 }
 
-module.exports = BinaryToDecimal;
+const binaryToDecimalRec = (binary) => {
+  const sum = Math.pow(2, binary.length - 1) * binary[0]
+  if (binary.length !== 1) {
+      return sum + binaryToDecimalRec(binary.slice(1));
+  }
+  return sum
+}
