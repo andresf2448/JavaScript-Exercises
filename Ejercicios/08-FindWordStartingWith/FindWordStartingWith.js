11ae4fef3f0ae37086c9474fe4b6074ee907528f
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-07-20 16:19:34 -0700

    08 finished

--- Ejercicios/08-FindWordStartingWith/FindWordStartingWith.js
+++ Ejercicios/08-FindWordStartingWith/FindWordStartingWith.js
@@ -1,51 +1,53 @@
 function FindWordStartingWith(book, query) {
-  // Your code here:
+    // Your code here:
+    
+    const text = book.text.toLowerCase();
+    const array = text.split("");
+    const position = [];
+    var result;
   
-  const text = book.text.toLowerCase();
-  const array = text.split("");
-  const position = [];
-  var result;
+    if(array[0] === query[0]){
+        
+        const longitud = query.length;
+        const corte = array.slice(0,longitud);
+        for(let i = 0; i < longitud; i++){
+            if(text[i] !== corte[i]){
+                result = false;
+            }
+        }
+        
+        if(result !== false){
+            position.push(0)
+        }
 
-  if(array[0] === query[0]){
-      
-      const longitud = query.length;
-      const corte = array.slice(0,longitud);
-      for(let i = 0; i < longitud; i++){
-          if(text[i] !== corte[i]){
-              result = false;
-          }
-      }
-      
-      if(result !== false){
-          position.push(0)
-      }
-
-      result = true;
-  }
-  
-  for(var j = query.length; j < text.length; j++){
-      if(text[j] === query[0] && text[j-1] === " "){
-          const longitud = query.length;
-          const corte = array.slice(j, j + longitud);
-          
-          for(let i = j; i < j + longitud; i++){
-              
-              if(text[i] !== corte[i - j]){
-                  result = false;
-              }
-          }
-          
-          if(result !== false){
-              position.push(j)
-              result = true;
-          }
-      }
-  }
-  return position;
+        result = true;
+    }
+    
+    for(var j = query.length; j < text.length; j++){
+        if(text[j] === query[0] && text[j-1] === " "){
+            const longitud = query.length;
+            const corte = array.slice(j, j + longitud);
+            
+            for(let i = j; i < j + longitud; i++){
+                
+                if(text[i] !== corte[i - j]){
+                    result = false;
+                }
+            }
+            
+            if(result !== false){
+                position.push(j)
+                result = true;
+            }
+        }
+    }
+    return position;
 }
 
-module.exports = FindWordStartingWith;
+/* Prueba */
+const book = {
+    id: 1,
+    text: 'Erase una vez un libro de palabras que era un poco aburrido pero tenia muchas'
+}
 
-
-"hola a todos"
-['holaatodos']
+console.log(FindWordStartingWith(book, "un"))
