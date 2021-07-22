f284ea022554b42eaeaaa3d1c016e8e320359662
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-07-21 17:15:27 -0700

    10 finished

--- Ejercicios/10-DecimalToBynary/DecimalToBynary.js
+++ Ejercicios/10-DecimalToBynary/DecimalToBynary.js
@@ -1,6 +1,20 @@
-function DecimalToBynary(num) {
-  // Your code here:
+/* primera forma */
 
+const decimalToBinaryRec = (num) => {
+  if (num) {
+      return decimalToBinaryRec(Math.floor(num / 2)) + num % 2
+  }
+  return '';
 }
 
-module.exports = DecimalToBynary;
+/* Segunda forma */
+
+function DecimalToBynary(num) {
+  let string = '';
+  while(num){
+    let modulo = num % 2;
+    string = modulo + string;
+    num = Math.floor(num/2);
+  }
+  return string;
+}
