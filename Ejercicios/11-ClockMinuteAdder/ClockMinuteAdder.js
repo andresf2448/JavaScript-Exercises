025d3d02c2ca20626322e52d40cd433e5aca397f
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-07-23 16:08:13 -0700

    11 finished

--- Ejercicios/11-ClockMinuteAdder/ClockMinuteAdder.js
+++ Ejercicios/11-ClockMinuteAdder/ClockMinuteAdder.js
@@ -1,6 +1,17 @@
 function clockMinuteAdder(time, minutesToAdd) {
   // Your code here:
+  const clockMinuteAdder = (time, minutesToAdd) => {
+    const [hours, minutes] = time.split(":");
 
+    const totalMinutes = minutesToAdd + parseInt(minutes);
+    const totalHours = Math.floor(totalMinutes / 60) + parseInt(hours);
+
+    const newHours = ((totalHours - 1) % 12) + 1;
+    const newMinutes = totalMinutes % 60;
+
+    const formatHours = newHours > 9 ? newHours : `0${newHours}`;
+    const formatMinutes = newMinutes > 9 ? newMinutes : `0${newMinutes}`;
+
+    return `${formatHours}:${formatMinutes}`;
+  };
 }
-
-module.exports = clockMinuteAdder
